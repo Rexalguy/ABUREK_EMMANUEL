25de9a507f861776ec6ee6ac1a02264c0622ee8c
Rexalguy <aburekemmanuel@gmail.com>
2026-06-11 13:45:59 +0300

    Control structures

--- basic.py
+++ basic.py
@@ -1,1 +1,5 @@
-print("Hello world")
+name = input("Enter your name: ")
+if name != "Manuel":
+    print("Sorry, only Manuel can use this program.")
+else:
+    print(f"Welcome, {name}! Let's split the bill.")
