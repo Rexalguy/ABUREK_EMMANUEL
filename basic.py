39e06824337f47a93936f3921a46e2312f6db855
Aburek Emmanuel <aburekemmanuel@gmail.com>
2026-06-13 12:37:22 +0300

    Add age checking functionality and update basic.py to include age prompt

--- basic.py
+++ basic.py
@@ -1,5 +1,12 @@
+from age import get_age
+
+
 name = input("Enter your name: ")
 if name != "Manuel":
     print("Sorry, only Manuel can use this program.")
 else:
     print(f"Welcome, {name}! Let's split the bill.")
+
+get_age()
+
+
